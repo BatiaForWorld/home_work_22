0d78957f224f7b04929b8a848539310db1491c97
andrei <dron_1985_85@mail.ru>
2025-11-14 01:09:13 +0800

    add custom commands

---
+++ catalog/management/commands/delete_db.py
@@ -0,0 +1,12 @@
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+from catalog.models import Category, Product
+
+
+class Command(BaseCommand):
+    help = 'Load test data from fixture'
+
+    def handle(self, *args, **kwargs):
+        # Удаляем существующие записи
+        Product.objects.all().delete()
+        Category.objects.all().delete()
